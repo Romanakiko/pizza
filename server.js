6ccfd9059f29ecddf9652f22ba13507c591695b8
Roman <roman.comm1@gmail.com>
2025-07-08 02:20:51 +0200

    fix deploy

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 // Serve static files from the Angular dist directory
-app.use(express.static(path.join(__dirname, 'dist/quiz/browser')));
+app.use(express.static(path.join(__dirname, 'dist/pizza/browser')));
 
 // For all GET requests, send back Angular's index.html file
 app.get('/*splat', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/quiz/browser/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/pizza/browser/index.html'));
 });
 
 app.use((req, res, next) => {
